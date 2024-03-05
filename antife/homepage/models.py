0ab066fa3ed727ba68d552b589e1c470c28f7dad
Rokanc <rokanc@gmail.com>
2024-03-05 18:55:04 +0200

    Produktu databases ikelta

--- antife/homepage/models.py
+++ antife/homepage/models.py
@@ -5,3 +5,14 @@ from django.db import models
 class TodoItem(models.Model):
     title = models.CharField(max_length = 200)
     completed = models.BooleanField(default = False)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    calories = models.DecimalField(max_digits=5, decimal_places=2)
+    total_fat = models.DecimalField(max_digits=5, decimal_places=2)
+    fiber = models.DecimalField(max_digits=5, decimal_places=2)
+    protein = models.DecimalField(max_digits=5, decimal_places=2)
+    phenylalanine = models.DecimalField(max_digits=5, decimal_places=3)
+    def __str__(self):
+        return self.name
+
