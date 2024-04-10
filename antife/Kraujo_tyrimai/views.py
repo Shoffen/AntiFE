cceb8c6f85c93f464065663513b0a1abc74b54d4
Shoffen <61907814+Shoffen@users.noreply.github.com>
2024-04-10 12:37:46 +0300

    Kraujo tyrimų surišimas su Naudotoju
    Padariau tam tikrį ryšį tarp naudotojo ir jo sukurto kraujo tyrimo.

--- antife/Kraujo_tyrimai/views.py
+++ antife/Kraujo_tyrimai/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from homepage.models import Kraujo_tyrimai, Naudotojai
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def create_kraujo_tyrimas(request):
+    if request.method == 'POST':
+        data = request.POST.get('data')
+        fenilalaninas = request.POST.get('fenilalaninas')
+        # Fetch the corresponding Naudotojai instance
+        naudotojai_instance = Naudotojai.objects.get(user=request.user)
+        # Create Kraujo Tyrimas for the current authenticated user
+        Kraujo_tyrimai.objects.create(data=data, fenilalaninas=fenilalaninas, fk_Naudotojasid_Naudotojas=naudotojai_instance)
+        return render(request, 'Kraujotyr.html')  # Redirect to some success page
+    return render(request, 'kraujo_tyrymai.html')
+
 
 def kraujotyrview(request):
     return render(request, 'Kraujotyr.html')
