6420f916dee4ff9b2ad819ae4f92cb49ce83ddd6
Shoffen <61907814+Shoffen@users.noreply.github.com>
2024-04-10 21:11:33 +0300

    Kraujo tyrimų grafikas

--- antife/Kraujo_tyrimai/utils.py
+++ antife/Kraujo_tyrimai/utils.py
@@ -12,6 +12,35 @@ def get_graph():
     buffer.close()
     return graph
 
+
+
+
+
 def get_plot(x, y):
-    graph = get_graph()
+    plt.switch_backend('AGG')
+    plt.figure(figsize=(10, 5))
+    plt.title('Kraujo tyrimai')
+    
+    # Plot the line
+    plt.plot(x, y, marker='o', linestyle='-')
+    
+    plt.xlabel('Data')
+    plt.ylabel('Fenilalaninas')
+    # Set x and y axis ticks to only include data corresponding to your points
+    plt.xticks(x)
+    plt.yticks(y)
+    
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+
+    # Save the plot to a buffer
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+
     return graph
+
